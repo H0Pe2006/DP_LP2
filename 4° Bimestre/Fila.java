1be3293e6c9ae03efadd63718890277767d2af48
Luiz Felipe <124799524+Leuis0101@users.noreply.github.com>
2023-10-06 17:53:44 -0300

    Update Fila.java

--- 4° Bimestre/Fila.java
+++ 4° Bimestre/Fila.java
@@ -1,11 +1,46 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Fila {
+public class Pilha {
 
     final static Scanner LER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String ingred = null;
+
+        int escolha = 0;
+
+        ArrayList<String> ingredientes = new ArrayList<String>();
+
+        do {
+            escolha = imprimirMenu();
+
+            if (escolha == 0) {
+                System.out.println("Até logo...!");
+                break;
+            }
+
+            switch (escolha) {
+                case 1:
+                    adicionarIng(ingredientes);
+                    break;
+                case 2:
+                    removerIng(ingredientes);
+                    break;
+                case 3:
+                    mostrarIng(ingredientes);
+                    break;
+                case 4:
+                    limparIng(ingredientes);
+                    break;
+                default:
+                    break;
+            }
+        } while (escolha != 0);
+
+    }
+
+    public static int imprimirMenu() {
+
         int escolha = 0;
 
         System.out.println("SANDUICHE MAKER");
@@ -15,51 +50,45 @@ public class Fila {
         System.out.println("Opção 3 - Mostrar os ingredientes");
         System.out.println("Opção 4 - Remover todos os ingredientes");
         System.out.println("Opção 0 - Sair");
+
         escolha = lerEscolha(escolha);
 
-        ArrayList<String> ingredientes = new ArrayList<String>();
-
-        do{
-        switch (escolha) {
-            case 0:
-                break;
-            case 1:
-                adicionarIng(ingredientes, ingred);
-                break;
-            case 2:
-                removerIng(ingredientes, ingred);;
-                break;
-            case 3:
-                mostrarIng(ingredientes);
-                break;
-            case 4:
-                limparIng(ingredientes, ingred);
-                break;
-            default:
-                break;
-        }
-    }while(escolha != 10);
-
+        return escolha;
     }
-    public static String lerIng(String ingred){
+
+    public static String lerIng() {
+        String ingred = null;
+        System.out.println("Digite o ingrediente: ");
         ingred = LER.next();
         return ingred;
     }
-    public static int lerEscolha(int escolha){
+
+    public static int lerEscolha(int escolha) {
         escolha = LER.nextInt();
         return escolha;
     }
-    public static void adicionarIng(ArrayList<String> ingredientes, String ingred){
-        ingredientes.add(lerIng(ingred));
-    }
-    public static void removerIng(ArrayList<String> ingredientes, String ingred){
-        ingredientes.remove(0);
-    }
-    public static void mostrarIng(ArrayList<String> ingredientes){
-        System.out.println(ingredientes);
-    }
-    public static void limparIng(ArrayList<String> ingredientes, String ingred){
-        ingredientes.clear();
+
+    public static void adicionarIng(ArrayList<String> ingredientes) {
+        String ingrediente = null;
+        ingrediente = lerIng();
+        ingredientes.add(ingrediente);
     }
 
+    public static void removerIng(ArrayList<String> ingredientes) {
+        ingredientes.remove(0);
+    }
+
+    public static void mostrarIng(ArrayList<String> ingredientes) {
+        System.out.println("");
+        System.out.println("Seu Sanduiche esta assim: ");
+        System.out.println(ingredientes);
+        System.out.println("");
+    }
+
+    public static void limparIng(ArrayList<String> ingredientes) {
+        System.out.println("");
+        System.out.println("Sanduiche descartado!");
+        ingredientes.clear();
+        System.out.println("");
+    }
 }
